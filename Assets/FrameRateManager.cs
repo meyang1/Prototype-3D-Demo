90d51789f0fd96d4f149c3973d41444184cf0bc9
meyang1 <meyang@stu.naperville203.org>
2022-03-14 03:13:07 -0500

    Revert "Revert "Adjusted Cam and Animations""
    This reverts commit 9c5e74cccec4d117b34af068b3509844ca30567a.

--- Assets/FrameRateManager.cs
+++ Assets/FrameRateManager.cs
@@ -6,7 +6,7 @@
      public int frameRate = 60;
  
      void Start() {
-         StartCoroutine(changeFramerate()); Application.targetFrameRate = 10;
+         StartCoroutine(changeFramerate()); Application.targetFrameRate = 60;
      }
      IEnumerator changeFramerate() {
          yield return new WaitForSeconds(1);
