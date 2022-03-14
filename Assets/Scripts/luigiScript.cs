3d8a4ed77bac6c42854ce8f9670eec78ab6109ec
meyang1 <meyang@stu.naperville203.org>
2022-03-14 03:11:27 -0500

    Revert "aa"
    This reverts commit d5b628ff4f5a2cb33efce9e570174f6c5575fb6c.

--- Assets/Scripts/luigiScript.cs
+++ Assets/Scripts/luigiScript.cs
@@ -56,13 +56,24 @@ public class luigiScript : MonoBehaviour
         else{
             speed = 3f;
         }
-
-
         if (Input.GetKey(KeyCode.Q))
         {
             Application.Quit();
         }
-        if (Input.GetKey(KeyCode.D))
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetInteger("AnimState", 3); //Left
+            //transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetInteger("AnimState", 3);
+            //transform.localRotation = Quaternion.Euler(0, 180, 0);
+        } 
+        
+        else if (Input.GetKey(KeyCode.D))
         {
             animator.SetInteger("AnimState", 1); //Right
             //transform.localRotation = Quaternion.Euler(0, 0, 0);
@@ -78,22 +89,6 @@ public class luigiScript : MonoBehaviour
             animator.SetInteger("AnimState", 0); //Idle
         }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            animator.SetInteger("AnimState", 1); //Left
-            //transform.localRotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            animator.SetInteger("AnimState", 1);
-            //transform.localRotation = Quaternion.Euler(0, 180, 0);
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
-        {  
-        }
-        else
-        {
-            animator.SetInteger("AnimState", 0);
-        }
+        
     }
 }
