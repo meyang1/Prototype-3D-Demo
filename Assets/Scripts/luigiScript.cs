38f84d0d8a4744302d61aafc70cb3c1eee7a37a5
meyang1 <meyang@stu.naperville203.org>
2022-03-14 02:49:23 -0500

    Adjusted Cam and Animations

--- Assets/Scripts/luigiScript.cs
+++ Assets/Scripts/luigiScript.cs
@@ -51,7 +51,7 @@ public class luigiScript : MonoBehaviour
         characterController.Move(moveDirection * Time.deltaTime);
 
         if(Input.GetKey(KeyCode.LeftShift)){
-            speed = 4.5f;
+            speed = 4f;
         }
         else{
             speed = 3f;
