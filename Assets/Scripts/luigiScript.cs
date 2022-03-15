f9dbe1fe96bd78e221cdd5e5ee16876d92b4c2c4
meyang1 <meyang@stu.naperville203.org>
2022-03-15 00:22:47 -0500

    Revert "Transition"
    This reverts commit 02e3c7a8f7184139d0222ea48e6059cdc7f67474.

--- Assets/Scripts/luigiScript.cs
+++ Assets/Scripts/luigiScript.cs
@@ -51,10 +51,10 @@ public class luigiScript : MonoBehaviour
         characterController.Move(moveDirection * Time.deltaTime);
 
         if(Input.GetKey(KeyCode.LeftShift)){
-            speed = 3.6f;
+            speed = 4f;
         }
         else{
-            speed = 2f;
+            speed = 3f;
         }
         if (Input.GetKey(KeyCode.Q))
         {
