9ccc9b3eccc3bc096d3b78ff3e86e59a646e17ce
meyang1 <meyang@stu.naperville203.org>
2022-04-07 14:54:28 -0500

    Removed Health

--- Assets/HealCharacter.cs
+++ Assets/HealCharacter.cs
@@ -6,15 +6,15 @@ public class HealCharacter : Interactable
     public override void Interact()
     {
         base.Interact();
-        if(PlayerManager.instance.player2.transform.GetComponent<SecondaryPlayerController>().focus != PlayerManager.instance.player2.transform.GetComponent<SecondaryPlayerController>().defaultFocus)
-        {
-            Heal(10);
-        }
-        else
-        {
+        //if(PlayerManager.instance.player2.transform.GetComponent<SecondaryPlayerController>().focus != PlayerManager.instance.player2.transform.GetComponent<SecondaryPlayerController>().defaultFocus)
+        //{
+         //   Heal(10);
+        //}
+        //else
+        //{
             Heal(30);
 
-        }
+       // }
     }
 
     void Heal(int healthAmount)
