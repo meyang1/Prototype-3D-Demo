a27e4e597c7a2336528872aa5c9ad9b829fe77a0
meyang1 <meyang@stu.naperville203.org>
2022-04-19 18:01:48 -0500

    All Edits

--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -30,6 +30,8 @@ public class Quest
         Debug.Log(title + " was completed!");
         goal = null;
         staticVars.questTypeWindow.SetActive(false);
+        staticVars.currencyAmount += currencyReward;
+        staticVars.experienceAmount += experienceReward;
     }
 
 }
