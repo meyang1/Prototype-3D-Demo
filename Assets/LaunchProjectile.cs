a220eb04e0f2c1b88d262562e64a221af7a4285d
meyang1 <meyang@stu.naperville203.org>
2022-05-01 09:53:06 -0500

    Ranged Bubbles

--- Assets/LaunchProjectile.cs
+++ Assets/LaunchProjectile.cs
@@ -5,28 +5,39 @@ using UnityEngine;
 public class LaunchProjectile : MonoBehaviour
 {
     public GameObject projectile;
-    public float launchVelocity = 700f;
+    public float launchVelocity = 700f;  
+    private float canShoot = 0f;
+    public float timeTillShoot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this.fixedDeltaTime = Time.fixedDeltaTime;
+        //timeTillShoot = Random.Range(.5f, 1.5f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(ShootBall(10f));
+        if (Time.time > canShoot)
+        {
+            StartCoroutine(ShootBall());
+            canShoot = Time.time + timeTillShoot;
+
+        }
         
     }
 
-    IEnumerator ShootBall(float timeDelay)
-    { 
-        yield return new WaitForSeconds(timeDelay);
+    IEnumerator ShootBall()
+    {
+        yield return null;//new WaitForSeconds(timeDelay);
 
         GameObject ball = Instantiate(projectile, transform.position,
                                              transform.rotation);
 
         ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3
                                          (0, launchVelocity, 0));
+
+        Destroy(ball, .4f);
     }
 }
