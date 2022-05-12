d992211b46428d75090c3658c352cf79e494e9d9
meyang1 <meyang@stu.naperville203.org>
2022-05-12 00:15:13 -0500

    The New Enemy !

--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyStats : CharacterStats
-{  
+{
+    public Animator ExtraDeathAnimation;
     public override void Die()
     {
         base.Die();
@@ -27,6 +28,8 @@ public class EnemyStats : CharacterStats
         Animator enemyAnimator = gameObject.GetComponent<Animator>();
         if (enemyAnimator != null)
             enemyAnimator.SetInteger("AnimState", 1);
+        if (ExtraDeathAnimation != null)
+            ExtraDeathAnimation.SetInteger("AnimState", 1);
         StartCoroutine(WaitToDie(1.4f));
 
         //add loot
